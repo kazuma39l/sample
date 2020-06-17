800ba9b3c8ae49170cec872ca6d5d4253004bf2b
kazuma39l <67031787+kazuma39l@users.noreply.github.com>
2020-06-17 16:06:25 +0900

    三人対戦２

--- main.ts
+++ main.ts
@@ -1,58 +1,12 @@
+let 相手の手 = 0
+let じゃんけん1 = 0
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.pause(1000)
-    if (じゃんけん1 == receivedNumber) {
-        basic.showIcon(IconNames.Asleep)
-        if (receivedNumber == !(receivedNumber == 1 || receivedNumber == 2)) {
-            if (じゃんけん1 == 1) {
-                basic.showIcon(IconNames.Happy)
-            } else if (じゃんけん1 == 2) {
-                basic.showIcon(IconNames.Angry)
-            }
-        }
-        if (receivedNumber == !(receivedNumber == 2 || receivedNumber == 3)) {
-            if (じゃんけん1 == 2) {
-                basic.showIcon(IconNames.Happy)
-            } else if (じゃんけん1 == 3) {
-                basic.showIcon(IconNames.Angry)
-            }
-        }
-        if (receivedNumber == !(receivedNumber == 1 || receivedNumber == 3)) {
-            if (じゃんけん1 == 1) {
-                basic.showIcon(IconNames.Angry)
-            } else if (じゃんけん1 == 2) {
-                basic.showIcon(IconNames.Happy)
-            }
-        }
-        if (receivedNumber == (receivedNumber == 1 && receivedNumber == 2)) {
-            if (じゃんけん1 == 1) {
-                basic.showIcon(IconNames.Angry)
-            } else if (じゃんけん1 == 2) {
-                basic.showIcon(IconNames.Happy)
-            }
-        }
-        if (receivedNumber == (receivedNumber == 2 && receivedNumber == 3)) {
-            if (じゃんけん1 == 2) {
-                basic.showIcon(IconNames.Angry)
-            } else if (じゃんけん1 == 3) {
-                basic.showIcon(IconNames.Happy)
-            }
-        }
-        if (receivedNumber == (receivedNumber == 1 && receivedNumber == 3)) {
-            if (じゃんけん1 == 1) {
-                basic.showIcon(IconNames.Happy)
-            } else if (じゃんけん1 == 3) {
-                basic.showIcon(IconNames.Angry)
-            }
-        }
-        if (じゃんけん1 == receivedNumber) {
-            basic.showIcon(IconNames.Asleep)
-        } else if (じゃんけん1 == 1 && (receivedNumber == 2 && receivedNumber == 3)) {
-            basic.showIcon(IconNames.Asleep)
-        } else if (じゃんけん1 == 2 && (receivedNumber == 1 && receivedNumber == 3)) {
-            basic.showIcon(IconNames.Asleep)
-        } else if (じゃんけん1 == 3 && (receivedNumber == 1 && receivedNumber == 2)) {
-            basic.showIcon(IconNames.Asleep)
-        }
+    if (receivedNumber == 1) {
+        相手の手 = 1
+    } else if (receivedNumber == 2) {
+        相手の手 = 2
+    } else {
+        相手の手 = 3
     }
 })
 input.onButtonPressed(Button.A, function () {
@@ -70,8 +24,61 @@ input.onButtonPressed(Button.B, function () {
     じゃんけん1 = 2
     radio.sendNumber(じゃんけん1)
 })
-let じゃんけん1 = 0
-radio.setGroup(9)
 basic.forever(function () {
-	
+    radio.setGroup(9)
+    basic.pause(500)
+    if (じゃんけん1 == 0) {
+        basic.showIcon(IconNames.Asleep)
+        if (相手の手 == 1) {
+            if (じゃんけん1 == 2) {
+                basic.showIcon(IconNames.Happy)
+            } else if (じゃんけん1 == 3) {
+                basic.showIcon(IconNames.Angry)
+            }
+        }
+        if (0 == 2) {
+            if (じゃんけん1 == 3) {
+                basic.showIcon(IconNames.Happy)
+            } else if (じゃんけん1 == 1) {
+                basic.showIcon(IconNames.Angry)
+            }
+        }
+    }
+    if (0 == 3) {
+        if (じゃんけん1 == 1) {
+            basic.showIcon(IconNames.Angry)
+        } else if (じゃんけん1 == 2) {
+            basic.showIcon(IconNames.Happy)
+        }
+    }
+    if (0 == 1 && 0 == 2) {
+        if (じゃんけん1 == 1) {
+            basic.showIcon(IconNames.Angry)
+        } else if (じゃんけん1 == 2) {
+            basic.showIcon(IconNames.Happy)
+        }
+    }
+    if (0 == 2 && 0 == 3) {
+        if (じゃんけん1 == 2) {
+            basic.showIcon(IconNames.Angry)
+        } else if (じゃんけん1 == 3) {
+            basic.showIcon(IconNames.Happy)
+        }
+    }
+    if (0 == (0 == 1 && 0 == 3)) {
+        if (じゃんけん1 == 1) {
+            basic.showIcon(IconNames.Happy)
+        } else if (じゃんけん1 == 3) {
+            basic.showIcon(IconNames.Angry)
+        }
+    }
+    if (じゃんけん1 == 0) {
+        basic.showIcon(IconNames.Asleep)
+    } else if (じゃんけん1 == 1 && (0 == 2 && 0 == 3)) {
+        basic.showIcon(IconNames.Asleep)
+    } else if (じゃんけん1 == 2 && (0 == 1 && 0 == 3)) {
+        basic.showIcon(IconNames.Asleep)
+    } else if (じゃんけん1 == 3 && (0 == 1 && 0 == 2)) {
+        basic.showIcon(IconNames.Asleep)
+    }
 })
